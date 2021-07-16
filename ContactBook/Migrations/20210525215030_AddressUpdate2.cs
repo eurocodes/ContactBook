bc6d56fdf544eb88159b921591fdd789b9fc72df
Ugo Umeoke <laryhug209@yahoo.com>
2021-07-16 11:49:51 +0100

    Add project files.

---
+++ ContactBook/Migrations/20210525215030_AddressUpdate2.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactBook.Migrations
+{
+    public partial class AddressUpdate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Addresses_AddressId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_AddressId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Contacts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactId",
+                table: "Addresses",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_ContactId",
+                table: "Addresses",
+                column: "ContactId",
+                unique: true,
+                filter: "[ContactId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Contacts_ContactId",
+                table: "Addresses",
+                column: "ContactId",
+                principalTable: "Contacts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Contacts_ContactId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_ContactId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "ContactId",
+                table: "Addresses");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressId",
+                table: "Contacts",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_AddressId",
+                table: "Contacts",
+                column: "AddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Addresses_AddressId",
+                table: "Contacts",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
